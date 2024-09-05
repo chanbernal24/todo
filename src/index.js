7621d0dfd7c464f65bff1778f574179d57ed624f
John Christian Bernal <bernal.johnchristian.08242003@gmail.com>
2024-09-05 17:22:46 +0800

    ui done, except with the todo modal.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import './styles.css'
 import './/scss/styles.scss'
 import * as bootstrap from 'bootstrap'
 import { Modal } from 'bootstrap'
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 
 console.log("Hello Worldssss!");
